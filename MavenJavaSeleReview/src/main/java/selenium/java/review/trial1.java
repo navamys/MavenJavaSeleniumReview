e55dab71e6f7362d75f83fd926c81c2068201dd9
Navamy <Navamy@Akhil-PC>
2024-09-26 10:12:34 +0530

    commiting on sept 26

---
+++ MavenJavaSeleReview/src/main/java/selenium/java/review/trial1.java
@@ -0,0 +1,13 @@
+package selenium.java.review;
+
+public class trial1 {
+	public static void display(String a) {
+		System.out.println(a);
+	}
+
+	public static void main(String[] args) {
+		trial1.display("Hello World");
+
+	}
+
+}
